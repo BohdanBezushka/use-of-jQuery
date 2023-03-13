e2865259e2d10ab786f0056852ed3eb8162f1d9a
Bohdan Bezushka Perepelytsia <bbezushka@gmail.com>
2023-03-13 21:34:42 +0000

    Complete the third exercise

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
     $("#stream1_btn").on("click", function() {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream1").addClass('highlight_stream');
+        $(".stream1").show("slow");
+        $(".stream1").show("medium");
+        $(".stream1").show("fast");
+        $(".stream1").show(1000);
+        $(".stream1").hide();
    });
    $("#stream2_btn").on("click", function() {
         $(".stream1").removeClass('highlight_stream');
@@ -18,3 +19,4 @@ $(document).ready(function() {
         $(".stream3").addClass('highlight_stream');
    });
 }); 
+
